fa7885317f96ef9c822b793c62691cd2cff8eb30
dcmox <dancmox@comcast.net>
2020-02-11 18:16:18 -0600

    export

--- tests/levenshtein.test.ts
+++ tests/levenshtein.test.ts
@@ -1,14 +1,14 @@
 import assert from 'assert'
 import sinon from 'sinon'
-import { levenshtein } from '../index'
+const levenshtein = require('../index')
 
 describe('levenshtein test suite', () => {
-    it('should calculate the distances between strings correctly', () => {
-        assert.equal(levenshtein('test', 'test'), 0)
-        assert.equal(levenshtein('', 'test'), 4)
-        assert.equal(levenshtein('test', ''), 4)
-        assert.equal(levenshtein('test', 'testing'), 3)
-        assert.equal(levenshtein('abc', 'def'), 3)
-        assert.equal(levenshtein('abc', 'cba'), 2)
-    })
+	it('should calculate the distances between strings correctly', () => {
+		assert.equal(levenshtein('test', 'test'), 0)
+		assert.equal(levenshtein('', 'test'), 4)
+		assert.equal(levenshtein('test', ''), 4)
+		assert.equal(levenshtein('test', 'testing'), 3)
+		assert.equal(levenshtein('abc', 'def'), 3)
+		assert.equal(levenshtein('abc', 'cba'), 2)
+	})
 })
